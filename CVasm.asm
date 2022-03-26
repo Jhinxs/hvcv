fbb6468382812488462a6aa5351c321941cc6463
陈枭 <79248856+axstorm@users.noreply.github.com>
2022-03-26 16:43:38 +0800

    hook

--- CVasm.asm
+++ CVasm.asm
@@ -217,8 +217,6 @@ VMXSaveRegState PROC
 		
 VMXSaveRegState ENDP
 
-;------------------------------------------------------------------------
-
 VMXRestoreRegState PROC
 	add rsp, 0100h
 
@@ -244,120 +242,6 @@ VMXRestoreRegState PROC
 VMXRestoreRegState ENDP
 
 
-vmx_vmmhostentry2 PROC
-    cli
-    push 0  ; we might be in an unaligned stack state, so the memory before stack might cause 
-            ; irql less or equal as it doesn't exist, so we just put some extra space avoid
-            ; these kind of erros
-
-   
-    pushfq
-    push r15
-    push r14
-    push r13
-    push r12
-    push r11
-    push r10
-    push r9
-    push r8        
-    push rdi
-    push rsi
-    push rbp
-    push rbp	; rsp
-    push rbx
-    push rdx
-    push rcx
-    push rax	
-
-	mov rcx, rsp		; Fast call argument to PGUEST_REGS
-	sub	rsp, 28h		; Free some space for Shadow Section
-	call	VmhostEntrydbg
-	add	rsp, 28h		; Restore the state
-
-	cmp	al, 1	; Check whether we have to turn off VMX or Not (the result is in RAX)
-	je		AsmVmxoffHandler2
-
-	RestoreState:
-	pop rax
-    pop rcx
-    pop rdx
-    pop rbx
-    pop rbp		; rsp
-    pop rbp
-    pop rsi
-    pop rdi 
-    pop r8
-    pop r9
-    pop r10
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop r15
-
-    popfq
-
-	sub rsp, 0100h      ; to avoid error in future functions
-    sti
-	vmresume
-
-vmx_vmmhostentry2 ENDP
-
-;------------------------------------------------------------------------
-
-AsmVmxoffHandler2 PROC
-    cli
-    sub rsp, 020h       ; shadow space
-    call CVReturnGuestRSP
-    add rsp, 020h       ; remove for shadow space
-
-    mov [rsp+088h], rax  ; now, rax contains rsp
-
-    sub rsp, 020h       ; shadow space
-    call CVReturnGuestRIP
-    add rsp, 020h       ; remove for shadow space
-
-    mov rdx, rsp        ; save current rsp
-
-    mov rbx, [rsp+088h] ; read rsp again
-
-    mov rsp, rbx
-
-    push rax            ; push the return address as we changed the stack, we push
-                        ; it to the new stack
-
-    mov rsp, rdx        ; restore previous rsp
-                        
-    sub rbx,08h         ; we push sth, so we have to add (sub) +8 from previous stack
-                        ; also rbx already contains the rsp
-    mov [rsp+088h], rbx ; move the new pointer to the current stack
-
-	RestoreState:
-
-	pop rax
-    pop rcx
-    pop rdx
-    pop rbx
-    pop rbp		         ; rsp
-    pop rbp
-    pop rsi
-    pop rdi 
-    pop r8
-    pop r9
-    pop r10
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop r15
-
-    popfq
-
-	pop		rsp     ; restore rsp
-    sti
-	ret             ; jump back to where we called Vmcall
-
-AsmVmxoffHandler2 ENDP
 
 
 vmx_vmmhostentry proc
@@ -431,19 +315,19 @@ vmxoffHandler PROC
     pop r14
     pop r15
 
-    sub rsp, 020h       ; shadow space
+    sub rsp, 020h       
     call CVReturnGuestRSP
-    add rsp, 020h       ; remove for shadow space
+    add rsp, 020h       
 
     mov rsp,rax 
 
-    sub rsp, 020h       ; shadow space
+    sub rsp, 020h       
     call CVReturnGuestRIP
-    add rsp, 020h       ; remove for shadow space
+    add rsp, 020h       
     
     push rax
 
-	ret             ; jump back to where we called Vmcall
+	ret            
 
 vmxoffHandler ENDP
 
